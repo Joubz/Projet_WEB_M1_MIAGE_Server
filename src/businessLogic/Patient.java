a6ad84e2ff46b044272684a43ac3ef060fe7c1d7
Julien Bascouzaraix <bascouzaraix.julien@gmail.com>
2020-05-24 19:44:27 +0200

    add maven to the project

---
+++ src/businessLogic/Patient.java
@@ -0,0 +1,132 @@
+package businessLogic;
+
+/**
+ * This class represents a patient and all his information
+ */
+public class Patient {
+
+    // Attributes
+
+    // TODO: Mettre en place un système d'id avec les xml
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String email;
+    private String dateOfBirth;
+    private char gender;
+    private int weight;
+    private int height;
+
+
+    // Constructors
+
+    /**
+     * Constructor by default of main.Patient class
+     */
+    public Patient() {
+    }
+
+    /**
+     * @param firstName   main.Patient first name
+     * @param lastName    main.Patient last name
+     * @param dateOfBirth main.Patient date of birth
+     * @param gender      main.Patient gender (F or M)
+     * @param weight      main.Patient weight (in kg unit)
+     * @param height      main.Patient height (in cm unit)
+     * @param phone       main.Patient phone number
+     * @param email       main.Patient email
+     */
+    public Patient(String firstName, String lastName, String dateOfBirth, char gender, int weight, int height,
+                   String phone, String email) {
+        try {
+            if (Utility.nameValidated(firstName, lastName) && Utility.phoneValidated(phone) &&
+                    Utility.emailValidated(email) && Utility.dateValidated(dateOfBirth)) {
+                this.firstName = firstName;
+                this.lastName = lastName;
+                this.phone = phone;
+                this.email = email;
+                this.dateOfBirth = dateOfBirth;
+                this.gender = gender;
+                this.weight = weight;
+                this.height = height;
+            } else
+                throw new Exception(new IllegalArgumentException());
+
+        } catch (Exception e) {
+            Log.WriteMessage(e);
+        }
+    }
+
+    /**
+     * Get the patient first name
+     *
+     * @return the patient first name
+     */
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    /**
+     * Get the patient last name
+     *
+     * @return the patient last name
+     */
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    /**
+     * Get the patient phone
+     *
+     * @return the patient phone
+     */
+    public String getPhone() {
+        return this.phone;
+    }
+
+    /**
+     * Get the patient email
+     *
+     * @return the patient email
+     */
+    public String getEmail() {
+        return this.email;
+    }
+
+    /**
+     * Get the patient date of birth
+     *
+     * @return the patient date of birth
+     */
+    public String getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    /**
+     * Get the patient gender (F = female, M = Male)
+     *
+     * @return the patient gender (F = female, M = Male)
+     */
+    public char getGender() {
+        return this.gender;
+    }
+
+    /**
+     * Get the patient weight (unit = kg)
+     *
+     * @return the weight of the patient
+     */
+    public int getWeight() {
+        return this.weight;
+    }
+
+    /**
+     * Get the patient height (unit = cm)
+     *
+     * @return the height of the patient
+     */
+    public int getHeight() {
+        return this.height;
+    }
+
+}
---
+++ src/test/ClassImplTest.java
@@ -0,0 +1,57 @@
+import businessLogic.Appointment;
+import businessLogic.Doctor;
+import businessLogic.Patient;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+/**
+ * Class where we test all class implementations
+ */
+public class ClassImplTest {
+
+    @Test
+    public final void testPatientClassImpl() {
+        Patient patient = new Patient("Julien", "Bascou", "20/09/1998",
+                'M', 68, 174, "0611026088", "bascou@gmail.com");
+
+        Assert.assertEquals("Julien", patient.getFirstName());
+        Assert.assertEquals("Bascou", patient.getLastName());
+        Assert.assertEquals("20/09/1998", patient.getDateOfBirth());
+        Assert.assertEquals('M', patient.getGender());
+        Assert.assertEquals(68, patient.getWeight());
+        Assert.assertEquals(174, patient.getHeight());
+        Assert.assertEquals("0611026088", patient.getPhone());
+        Assert.assertEquals("bascou@gmail.com", patient.getEmail());
+
+    }
+
+    @Test
+    public final void testDoctorClassImpl() {
+        Doctor doctor = new Doctor("Bascou", "Julien", "Généraliste",
+                "1 rue du paradis");
+
+        Assert.assertEquals("Julien", doctor.getFirstName());
+        Assert.assertEquals("Bascou", doctor.getLastName());
+        Assert.assertEquals("Généraliste", doctor.getSpeciality());
+        Assert.assertEquals("1 rue du paradis", doctor.getAddress());
+    }
+
+    @Test
+    public final void testAppointmentClassImpl() {
+        Patient patient = new Patient("Julien", "Bascou", "20/09/1998",
+                'M', 68, 174, "0611026088", "bascou@gmail.com");
+        Doctor doctor = new Doctor("Bascou", "Julien", "Généraliste",
+                "1 rue du paradis");
+
+        Appointment appointment = new Appointment(doctor, patient, "20/09/2020", "17-17h30");
+
+        Assert.assertEquals(doctor, appointment.getDoctor());
+        Assert.assertEquals(patient, appointment.getPatient());
+        Assert.assertEquals("20/09/2020", appointment.getDate());
+        Assert.assertEquals("17-17h30", appointment.getTime());
+
+    }
+
+
+}
